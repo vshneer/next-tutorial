a66d1cc10e9cc2a311fcb6f2d72c0350a64ba9df
Victor Shneer <victor.shneer@gmail.com>
2025-03-19 19:00:44 +0200

    Fix Next.js font preload error

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
-import { Inter } from 'next/font/google';
-import { Lusitana } from 'next/font/google';
+import {Inter} from 'next/font/google';
+import {Lusitana} from 'next/font/google';
 
-export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({ weight: "400"});
+export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana({subsets: ['latin'], weight: "400"});
